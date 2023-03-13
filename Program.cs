5490634953f39e9a693bc3b03a605ec562c78bde
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-13 18:59:21 +0530

    Returns a custom Exception

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
-﻿using static IndianStateCensusAnalyserProblem.Analyser;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace IndianStateCensusAnalyserProblem
+namespace TestProject
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            
-    
+            try
+            {
+                var csv_data = File.ReadAllLines("C:\\Users\\Admin\\source\\repos\\214\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\statecensus.csv").Skip(1);
+                Console.WriteLine(csv_data.Count());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
