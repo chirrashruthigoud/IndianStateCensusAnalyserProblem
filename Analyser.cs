d4d28813b922ac9ab14737399c8325f42b64e383
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-13 18:58:44 +0530

    Returns a custom Exception

--- Analyser.cs
+++ Analyser.cs
@@ -10,39 +10,10 @@ namespace IndianStateCensusAnalyserProblem
     {
 
         private readonly string csv_file = "C:\\Users\\Admin\\source\\repos\\214\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\statecensus.csv";
-
         public Analyser(string csv_file)
         {
             this.csv_file = csv_file;
         }
 
-        public int GetRecordCount()
-        {
-            try
-            {
-                var csv_data = File.ReadAllLines("C:\\Users\\Admin\\source\\repos\\214\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\statecensus.csv").Skip(1);
-                return csv_data.Count();
-            }
-            catch (FileNotFoundException)
-            {
-                throw new ArgumentException("File not found.");
-            }
-        }
-    }
-
-    public class StateCensusAnalyser
-    {
-        private readonly string csv_file = "C:\\Users\\Admin\\source\\repos\\214\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\statecensus.csv";
-
-        public StateCensusAnalyser(string csv_file)
-        {
-            this.csv_file = csv_file;
-        }
-
-        public int GetRecordCount()
-        {
-            var csv_data = new Analyser("C:\\Users\\Admin\\source\\repos\\214\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\statecensus.csv").GetRecordCount();
-            return csv_data;
-        }
     }
 }
