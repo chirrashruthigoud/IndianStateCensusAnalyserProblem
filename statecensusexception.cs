1e0d02c73a4613f9d4a1ae7a25cdb05161eff2aa
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-13 18:58:52 +0530

    Returns a custom Exception

---
+++ statecensusexception.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyserProblem
+{
+    public class statecensusexception
+    {
+        public statecensusexception(string message) : base(message) { }
+
+    }
+}
